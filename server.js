a5a27db5f426b7b63b894772ed37dc4498ea78c4
nnm0122 <nnm0122@gmail.com>
2018-02-03 10:02:36 -0500

    adding routes folder and updating files

--- server.js
+++ server.js
@@ -1,49 +1,122 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose");
+const express = require("express");
+const bodyParser = require("body-parser");
+// const logger = require("morgan");
+const mongoose = require("mongoose");
 
-var PORT = 3000;
-
-// Requiring the `User` model for accessing the `users` collection
-var User = require("./userModel.js");
-
-// Initialize Express
+//listen on port
+var port = process.env.PORT || 3000;
+//Initialize Express
 var app = express();
-
-// Configure middleware
-
-// Use morgan logger for logging requests
-app.use(logger("dev"));
-// Use body-parser for handling form submissions
-app.use(bodyParser.urlencoded({ extended: false }));
-// Use express.static to serve the public folder as a static directory
+// Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static("public"));
 
-// Set mongoose to leverage built in JavaScript ES6 Promises
-// Connect to the Mongo DB
+
+
+// require all models
+const db =  require("./src/models");
+
+var routes = require("./routes/routes.js");
+app.use("/routes", routes);
+
+// Middleware
+// app.use(logger("dev"));
+app.use(bodyParser.json());
+// app.use(express.static("public"));
+
 mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/nytreact", {
-  useMongoClient: true
+mongoose.connect("mongodb://localhost/NYTreact", {
 });
 
-// Routes
 
-// Route to post our form submission to mongoDB via mongoose
-app.post("/submit", function(req, res) {
-  // Create a new user using req.body
-  User.create(req.body)
-    .then(function(dbUser) {
-      // If saved successfully, send the the new User document to the client
-      res.json(dbUser);
-    })
-    .catch(function(err) {
-      // If an error occurs, send the error to the client
-      res.json(err);
-    });
-});
+//routes
 
-// Start the server
-app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
-});
+// app.get("/articles", (req, res) => {
+    
+//     db.Article.find({}).then(dbArticle => res.json(dbArticle))
+    
+//     .catch(err => res.json(err))
+// });
+
+// app.post("/articles", (req, res) => {
+// console.log(req.body);
+//     db.Article.create({
+//      url: req.body.url, 
+//      title: req.body.title, 
+//      snippet: req.body.snippet})
+
+//     .then(dbArticle => res.json(dbArticle))
+
+//     .catch(err => res.json(err))
+// });
+
+
+
+//    res.render("index", {tester: '309'});
+//  // res.render("./layouts/main", {homeClass: 'class="active"'});
+//  // res.render("./layouts/main", {listClass: 'class=""'});
+// });
+
+// app.get("/list", function(req, res, next) {
+//  res.render("list", {tester: '520'});
+//  // res.render ("./layouts/main", {listClass: 'class="active"'});
+//  // res.render ("./layouts/main", {homeClass: 'class=""'});
+// });
+
+// app.get("/scrape", function(req, res){
+//  console.log("i am scraping");
+//  request("https://www.nytimes.com/", function(error, response, html){
+//    var $ = cheerio.load(html);
+
+//    var results = [];
+//    // console.log($);
+//    $("article.story").each(function(i, element) {
+//      var url =$(element).children("h2.story-heading").children("a").attr("href");
+//      var headline =$(element).children("h2.story-heading").children("a").text().trim();
+//      var summary =$(element).children("p.summary").text().trim();
+
+//      // console.log(i);
+//      // console.log("URL for article " + url);
+//      // console.log("headline for Article ", headline);
+//      // console.log("Summary: ", summary);
+
+//      var article = {
+//        url: url,
+//        headline: headline,
+//        summary: summary
+//        };
+
+//      var entry = new db.Article(article);
+//      entry.save(function(error, data){
+//        if (error) console.log(error)
+//          // console.log(data)
+//      });
+
+//    // console.log("consolidated: ", article);
+//    // res.render("index", article);
+//    });
+//  db.Article.find({}).populate("note").exec(function(error, data){
+//      if (error) console.log(error)
+//            console.log(data)
+//          res.render("index", {
+//            scrape:data
+//          })
+//    })
+//  });
+
+// });
+
+// Initiate the listener.
+app.listen(port);
+console.log("App running on port " + port + "!");
+
+// app.post("/save/:id", function(req, res) {
+//     var id = req.param.id
+//     db.Article.update({
+//         saved: true,
+//         where: {
+//             _id: id
+//         }
+//     }).then(function(err, data) {
+// if (err) console.log(err)
+// });
+// })
---
+++ src/pages/Homepage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Jumbotron } from '../components/Jumbotron';
+
+export const Homepage = () => {
+	return
+	<div>
+		<Jumbotron
+			title="Search New York Times"
+			subTitle="Search by Subject in a given period of time." />
+
+{/*
+// props for search - 
+//		gathers users data and searches NYT
+//		displays results in Results
+//		gives button for savinging
+//			button saves to DB
+
+//props for results
+//		gets all saved articles from DB with option to delete button
+//		delete button removes from DB
+*/}
+
+	</div>	
+}
